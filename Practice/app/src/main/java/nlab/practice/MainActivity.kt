110dc0892d657213ec17a71de0532dc617cfa994
Doohyun Nam <skaengus2012@naver.com>
2018-04-11 13:46:01 +0900

    프로젝트 첨부.

---
+++ Practice/app/src/main/java/nlab/practice/MainActivity.kt
@@ -0,0 +1,12 @@
+package nlab.practice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
