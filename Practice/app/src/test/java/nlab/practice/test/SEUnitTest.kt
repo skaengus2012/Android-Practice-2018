9fd1b967c3ce60eaa1d289bde18facbfb49fe325
Doohyun Nam <skaengus2012@naver.com>
2018-04-11 17:58:25 +0900

    #1 Hello World 추가.

---
+++ Practice/app/src/test/java/nlab/practice/test/SEUnitTest.kt
@@ -0,0 +1,23 @@
+package nlab.practice.test
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * 코틀린 기본 문법에 대한 테스트 코드 기술.
+ *
+ * Standard 프로젝트.
+ */
+class SEUnitTest {
+
+    /**
+     * Print "Hello World"
+     */
+    @Test
+    fun runHelloWorld() {
+        println("Hello World!")
+    }
+
+
+}
