9ce1583b72e755f370d7e57a866a8cf830726eee
Doohyun Nam <skaengus2012@naver.com>
2018-04-15 15:36:59 +0900

    #4 꼬리재귀 키워드 테스트 제작.

--- Practice/app/src/test/java/nlab/practice/test/KeywordChat2UnitTest.kt
+++ Practice/app/src/test/java/nlab/practice/test/KeywordChat2UnitTest.kt
@@ -1,5 +1,7 @@
 package nlab.practice.test
 
+import org.junit.Test
+
 /**
  * Kotlin keyword 관련 추가적인 테스트 슈트 정의
  *
@@ -9,5 +11,33 @@ package nlab.practice.test
  * @author ndh1002
  */
 class KeywordChat2UnitTest {
+
+    /**
+     * 재귀함수 최적화 테스트.
+     */
+    @Test
+    fun testRecursive() {
+        val factorialResult = getFactorialResult(5)
+        println(factorialResult)
+    }
+
+    /**
+     * 팩토리얼 구현.
+     *
+     * [number] 에 해당하는 팩토리얼 값 계산.
+     *
+     * tailrec 키워드 사용 시, 스택오버플로 걱정이 없는 빠르고 효율적인 루프 기반 버전으로 컴파일러가 최적화해준다.
+     *
+     * @param number
+     */
+    private tailrec fun getFactorialResult(number : Int) : Int {
+
+        check(number >= 1, {"잘못된 접근입니다."})
+
+        return when(number) {
+            1 -> 1
+            else -> number * getFactorialResult(number - 1)
+        }
+    }
 
 }
