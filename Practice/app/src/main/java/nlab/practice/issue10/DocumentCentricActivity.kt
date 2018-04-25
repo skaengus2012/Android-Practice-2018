59c6071a6d1f298de84f09b883dd01692d810588
Doohyun Nam <skaengus2012@naver.com>
2018-04-25 09:20:10 +0900

    #10 개요화면 화면추가 처리까지 완료~

--- Practice/app/src/main/java/nlab/practice/issue10/DocumentCentricActivity.kt
+++ Practice/app/src/main/java/nlab/practice/issue10/DocumentCentricActivity.kt
@@ -1,7 +1,9 @@
 package nlab.practice.issue10
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_document_centric.*
 import nlab.practice.R
 
 /**
@@ -13,8 +15,31 @@ import nlab.practice.R
  */
 class DocumentCentricActivity : AppCompatActivity() {
 
+    private var documentCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_document_centric)
+
+        // 새 도큐먼트 이벤트 바인딩.
+        btnNewDocument.setOnClickListener({ startActivity(createNewDocumentIntent()) })
+
+        // 새 도큐먼트에 멀티 태스크가 추가된 형태로 처리.
+        btnNewDocumentWithMultiTask.setOnClickListener({
+            createNewDocumentIntent()
+                    .addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+                    .let { startActivity(it) }
+        })
     }
+
+    /**
+     * FLAG_ACTIVITY_NEW_DOCUMENT 플래그를 가지며, NewDocumentActivity 를 실행시킬 수 있는 명시적 인텐트를 생산한다.
+     *
+     * @return
+     */
+    private fun createNewDocumentIntent(): Intent =
+            Intent(this, NewDocumentActivity::class.java).apply {
+                this.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
+                this.putExtra(NewDocumentActivity.COUNT_EXTRA, (++documentCount).toString())
+            }
 }
--- Practice/app/src/main/java/nlab/practice/issue10/NewDocumentActivity.kt
+++ Practice/app/src/main/java/nlab/practice/issue10/NewDocumentActivity.kt
@@ -1,7 +1,10 @@
 package nlab.practice.issue10
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_new_document.*
 import nlab.practice.R
 
 /**
@@ -11,8 +14,41 @@ import nlab.practice.R
  */
 class NewDocumentActivity : AppCompatActivity() {
 
+    companion object {
+        const val COUNT_EXTRA = "COUNT_EXTRA"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_document)
+
+        setTextByCount(intent)
+        tvAlreadyCreated.visibility = View.GONE
+    }
+
+    /**
+     * 새로운 [intent] 가 들어온다면, 이미 생성되었다는 라벨을 표시한다.
+     *
+     * @param intent
+     */
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setTextByCount(intent)
+        tvAlreadyCreated.visibility = View.VISIBLE
+    }
+
+    /**
+     * [intent] 로부터 카운트를 추출하여, 텍스트뷰에 세팅한다.
+     *
+     * @param intent
+     */
+    private fun setTextByCount(intent: Intent?) {
+        val countStr = intent?.getStringExtra(COUNT_EXTRA)
+        with(countStr) {
+            checkNotNull(this)
+
+            tvCount.text = this!!
+        }
     }
 }
