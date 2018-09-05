147a533b7a4beea2fec59448a8b8b581ed499085
Doohyun <skaengus2012@naver.com>
2018-09-05 10:15:45 +0900

    코드 정리

--- Practice/app/src/main/java/nlab/practice/issue33/SharedElementTargetActivity.kt
+++ Practice/app/src/main/java/nlab/practice/issue33/SharedElementTargetActivity.kt
@@ -11,25 +11,28 @@ import kotlinx.android.synthetic.main.activity_shared_element_target.*
 import nlab.practice.R
 import nlab.practice.util.GlideApp
 
+// 조회할 이미지
+private const val IMAGE_URL = "http://thumbnail.egloos.net/600x0/http://pds21.egloos.com/pds/201805/15/21/f0041321_5afaeddcd6541.jpg"
+
 /**
  * SharedCurrentElement 를 눌렀을 때, 나타날 화면
  *
  * @author Doohyun
  */
-private const val IMAGE_URL = "http://thumbnail.egloos.net/600x0/http://pds21.egloos.com/pds/201805/15/21/f0041321_5afaeddcd6541.jpg"
-
 class SharedElementTargetActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_element_target)
 
+        // SHARED ELEMENT 수행 중, async image loading 에 대한 처리
         supportPostponeEnterTransition()
 
         GlideApp.with(this)
                 .load(IMAGE_URL)
                 .dontAnimate()
                 .listener(object : RequestListener<Drawable> {
+
                     override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
                         supportStartPostponedEnterTransition()
 
